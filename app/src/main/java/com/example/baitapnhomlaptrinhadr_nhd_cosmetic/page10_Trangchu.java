75d0d9e1e9f16a64849b3cf1bf677874a29390c0
LeHoa1206 <hoadt122@gmail.com>
2024-10-09 19:29:07 +0700

    Lehoaupdate

---
+++ app/src/main/java/com/example/baitapnhomlaptrinhadr_nhd_cosmetic/page10_Trangchu.java
@@ -0,0 +1,12 @@
+package com.example.baitapnhomlaptrinhadr_nhd_cosmetic;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page10_Trangchu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_10_trangchu);  // Đảm bảo bạn có file XML với tên này
+    }
+}
--- app/src/main/java/com/example/baitapnhomlaptrinhadr_nhd_cosmetic/page8_Trangchu.java
+++ app/src/main/java/com/example/baitapnhomlaptrinhadr_nhd_cosmetic/page8_Trangchu.java
@@ -2,29 +2,27 @@ package com.example.baitapnhomlaptrinhadr_nhd_cosmetic;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class page8_Trangchu extends AppCompatActivity {
-    Button btnforward;
+   ImageView btnhomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page8_cosmetic);  // Đảm bảo layout này tồn tại
-
-        // Tìm nút "Tiếp theo" bằng ID
-//        btnforward = findViewById(R.id.btnforward);  // Đảm bảo bạn có nút với id btnforward trong layout
-//
-//        // Xử lý sự kiện cho nút "Tiếp theo"
-//        btnforward.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Intent chuyển từ page8_Trangchu sang một activity khác
-//                Intent intent = new Intent(page8_Trangchu.this, page4_khamphangay.class);
-//                startActivity(intent);
-//            }
-//        });
+        btnhomeButton=findViewById(R.id.btnhome);
+        btnhomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(page8_Trangchu.this,page10_Trangchu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
